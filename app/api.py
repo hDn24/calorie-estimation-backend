17bd54c4dbf78f3dcbd0afd2520ebc836eb3673e
Hồ Đăng Nhân <51161577+hDn24@users.noreply.github.com>
2023-04-18 06:50:43 +0000

    Add new api

---
+++ app/api.py
@@ -0,0 +1,37 @@
+import os
+import uuid
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from utils import utils
+
+
+def detection(files, detector):
+    output_folder = "static"
+    response = []
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+
+    for file_name in files:
+        image = Image.open(file_name)
+        tensor_image = np.asarray(image)
+
+        # Run object detection using the model.
+        detections = detector.detect(tensor_image)
+
+        # Draws bounding boxes on the input image
+        output_image = utils.visualize(tensor_image, detections)
+
+        # Convert to RGB
+        output_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB)
+
+        # Save output image
+        output_image_name = f"{str(uuid.uuid4())}.png"
+        output_image_path = output_folder + "/" + output_image_name
+        cv2.imwrite(output_image_path, output_image)
+
+        response.append(output_image_path)
+
+    return response
